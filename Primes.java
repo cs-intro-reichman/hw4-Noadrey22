083623855d6fbc8995771c4bf9558d12a43591cb
Noa <noa.drey@post.runi.ac.il>
2024-12-04 19:55:06 +0200

    matala4try1

--- Primes.java
+++ Primes.java
@@ -1,5 +1,31 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int input = Integer.parseInt(args[0]);
+        boolean[] arr = new boolean[input + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = true;
+        }
+        arr[0] = false;
+        arr[1] = false;
+        int index = 0;
+        while (index < arr.length) {
+            while (index < arr.length && arr[index] == false) {
+                index++;
+            }
+            int index2 = index + 1;
+            while (index2 < arr.length) {
+                if (index2 % index == 0) {
+                    arr[index2] = false;
+                }
+                index2++;
+
+            }
+            index++;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == true) {
+                System.out.println(i);
+            }
+        }
     }
 }
